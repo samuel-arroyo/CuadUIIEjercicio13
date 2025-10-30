5e134bf4bd1fc96944f31e1762f88b601c60c890
Samuel Arroyo <42401837@uaz.edu.mx>
2025-10-30 19:56:44 +0100

    Creacion del codigo en el main

--- src/cuaduiiejercicio13/CuadUIIEjercicio13.java
+++ src/cuaduiiejercicio13/CuadUIIEjercicio13.java
@@ -15,7 +15,36 @@ public class CuadUIIEjercicio13 {
      */
     public static void main(String[] args) 
     {
-        
+        Car miAuto = new Car("Toyota ", 2022);
+
+        // Estado inicial
+        miAuto.imprimirEstado();
+
+        // Intentar acelerar sin encender
+        miAuto.acelerar(20);
+
+        // Encender el auto
+        miAuto.encender();
+        miAuto.imprimirEstado();
+
+        // Acelerar
+        miAuto.acelerar(50);
+        miAuto.imprimirEstado();
+
+        // Frenar
+        miAuto.frenar(30);
+        miAuto.imprimirEstado();
+
+        // Intentar apagar mientras está en movimiento
+        miAuto.apagar();
+
+        // Frenar completamente
+        miAuto.frenar(20);
+        miAuto.imprimirEstado();
+
+        // Apagar correctamente
+        miAuto.apagar();
+        miAuto.imprimirEstado();
     
     }
     
